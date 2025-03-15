67916cc11a1d5e92c843b080376649a17c315a15
SojiJoro <71898347+SojiJoro@users.noreply.github.com>
2025-03-15 18:04:10 +0000

    Add ignoreDuringBuilds to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Dangerously allow production builds to complete even if there are ESLint errors
+    ignoreDuringBuilds: true,
+  },
+  // Add any other Next.js config options here, for example:
+  // reactStrictMode: true,
 };
 
 export default nextConfig;
