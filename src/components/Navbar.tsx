f22cfeece206b6e4ae426b112d7d12e140342a2e
SojiJoro <71898347+SojiJoro@users.noreply.github.com>
2025-03-15 22:23:29 +0000

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
 
 export default function Navbar() {
   const [activeDropdown, setActiveDropdown] = useState("");
@@ -11,40 +11,81 @@ export default function Navbar() {
   }
 
   return (
-    <nav className="navbar navbar-expand-lg fixed-top" id="mainNav">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="mainNav">
       <div className="container d-flex justify-content-between align-items-center">
         {/* Logo */}
         <Link className="navbar-brand d-flex align-items-center" href="/">
-          <Image src="/images/logo.png" alt="Clear Vital Logo" width={50} height={50} className="logo-stroke me-2" />
+          <Image
+            src="/images/logo.png"
+            alt="Clear Vital Logo"
+            width={50}
+            height={50}
+            className="logo-stroke me-2"
+          />
           <span className="fw-bold">Clear Vital Social Care</span>
         </Link>
 
         {/* Hamburger Button */}
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
 
         {/* Navigation Menu */}
         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link active" href="/">Home</Link>
+            <li className="nav-item me-3">
+              <Link className="nav-link" href="/">
+                Home
+              </Link>
             </li>
 
             {/* Who We Are */}
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" onClick={() => handleToggleDropdown("whoWeAre")}>
+            <li className="nav-item dropdown me-3">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault(); // Prevent jump to top
+                  handleToggleDropdown("whoWeAre");
+                }}
+              >
                 Who We Are
               </a>
               {activeDropdown === "whoWeAre" && (
                 <ul className="dropdown-menu show">
-                  <li><Link className="dropdown-item" href="/who-we-are/mission">Our Mission</Link></li>
-                  <li><Link className="dropdown-item" href="/who-we-are/history">Our History</Link></li>
-                  <li><Link className="dropdown-item" href="/who-we-are/values">Our Values</Link></li>
-                  <li><Link className="dropdown-item" href="/who-we-are/team">Our Team</Link></li>
+                  <li>
+                    <Link className="dropdown-item" href="/who-we-are/mission">
+                      Our Mission
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="/who-we-are/history">
+                      Our History
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="/who-we-are/values">
+                      Our Values
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="/who-we-are/team">
+                      Our Team
+                    </Link>
+                  </li>
                 </ul>
               )}
             </li>
+
+            {/* ...other dropdowns... */}
           </ul>
         </div>
       </div>
