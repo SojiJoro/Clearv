1864ed0901df63b2a17e0f9c80136e726db6a669
SojiJoro <71898347+SojiJoro@users.noreply.github.com>
2025-03-15 18:23:43 +0000

    Add valid default exports for new page.tsx files

---
+++ src/app/how-you-can-help/corporate-partnerships/page.tsx
@@ -0,0 +1,9 @@
+export default function CorporatePartnerPage() {
+    return (
+      <div>
+        <h1>Corporate Partnerships</h1>
+        <p>We collaborate with businesses to expand our reach and better serve local communities.</p>
+      </div>
+    );
+  }
+  
