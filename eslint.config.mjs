560ba09881e7d044e48094285b156d24ac27b201
SojiJoro <71898347+SojiJoro@users.noreply.github.com>
2025-03-15 17:29:26 +0000

    Write a short descriptive message here

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,18 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// This array is your entire ESLint config
 const eslintConfig = [
+  // Bring in the default Next.js + TypeScript ESLint settings
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Add your own custom rules here
+  {
+    rules: {
+      "react/no-unescaped-entities": "warn",
+      // You can override other rules here if needed
+    },
+  },
 ];
 
 export default eslintConfig;
