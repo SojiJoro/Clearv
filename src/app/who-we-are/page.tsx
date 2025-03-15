abb75359f97b2ed81cd955fd776d2dbb90c087c1
SojiJoro <71898347+SojiJoro@users.noreply.github.com>
2025-03-15 18:25:24 +0000

    Add valid default exports for new page.tsx files

---
+++ src/app/who-we-are/page.tsx
@@ -0,0 +1,9 @@
+export default function Whoweare() {
+    return (
+      <div>
+        <h1>Who we are</h1>
+        <p>Learn about the various services we provide to support individuals in the community.</p>
+      </div>
+    );
+  }
+  
