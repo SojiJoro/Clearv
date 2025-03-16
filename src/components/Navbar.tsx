43587dd0c9e6840b4a58cd6d05e41b68ac9239e5
SojiJoro <71898347+SojiJoro@users.noreply.github.com>
2025-03-16 17:50:30 +0000

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,8 +6,7 @@ import Image from "next/image";
 
 export default function Navbar() {
   useEffect(() => {
-    // Load Bootstrap JS if needed (e.g., for modals).
-    // The dropdown is purely hover-based via CSS, so no data-bs-toggle here.
+    // Load Bootstrap JS to enable dropdown toggling on mobile
     if (typeof window !== "undefined") {
       require("bootstrap/dist/js/bootstrap.bundle.min.js");
     }
@@ -53,77 +52,122 @@ export default function Navbar() {
               </Link>
             </li>
 
-            {/* Who We Are (Hover Dropdown) */}
+            {/* Who We Are (Hybrid Dropdown) */}
             <li className="nav-item dropdown me-3">
-              <Link href="#" passHref>
-                <a className="nav-link dropdown-toggle">Who We Are</a>
-              </Link>
-              <div className="dropdown-menu">
-                <Link href="/who-we-are/mission" passHref>
-                  <a className="dropdown-item">Our Mission</a>
-                </Link>
-                <Link href="/who-we-are/history" passHref>
-                  <a className="dropdown-item">Our History</a>
-                </Link>
-                <Link href="/who-we-are/values" passHref>
-                  <a className="dropdown-item">Our Values</a>
-                </Link>
-                <Link href="/who-we-are/team" passHref>
-                  <a className="dropdown-item">Our Team</a>
-                </Link>
-              </div>
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Who We Are
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <Link href="/who-we-are/mission" passHref>
+                    <a className="dropdown-item">Our Mission</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/who-we-are/history" passHref>
+                    <a className="dropdown-item">Our History</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/who-we-are/values" passHref>
+                    <a className="dropdown-item">Our Values</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/who-we-are/team" passHref>
+                    <a className="dropdown-item">Our Team</a>
+                  </Link>
+                </li>
+              </ul>
             </li>
 
-            {/* What We Do (Hover Dropdown) */}
+            {/* What We Do (Hybrid Dropdown) */}
             <li className="nav-item dropdown me-3">
-              <Link href="#" passHref>
-                <a className="nav-link dropdown-toggle">What We Do</a>
-              </Link>
-              <div className="dropdown-menu">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                What We Do
+              </a>
+              <ul className="dropdown-menu">
                 {/* Overview link to /what-we-do */}
-                <Link href="/what-we-do" passHref>
-                  <a className="dropdown-item">Overview</a>
-                </Link>
-                <Link href="/what-we-do/adult-social-care" passHref>
-                  <a className="dropdown-item">Adult Social Care</a>
-                </Link>
-                <Link href="/what-we-do/children-families" passHref>
-                  <a className="dropdown-item">Children & Families</a>
-                </Link>
-                <Link href="/what-we-do/supported-living" passHref>
-                  <a className="dropdown-item">Supported Living</a>
-                </Link>
-                <Link href="/what-we-do/residential-care" passHref>
-                  <a className="dropdown-item">Residential Care</a>
-                </Link>
-                <Link href="/what-we-do/day-services" passHref>
-                  <a className="dropdown-item">Day Services</a>
-                </Link>
-                <Link href="/what-we-do/education-training" passHref>
-                  <a className="dropdown-item">Education & Training</a>
-                </Link>
-              </div>
+                <li>
+                  <Link href="/what-we-do" passHref>
+                    <a className="dropdown-item">Overview</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/what-we-do/adult-social-care" passHref>
+                    <a className="dropdown-item">Adult Social Care</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/what-we-do/children-families" passHref>
+                    <a className="dropdown-item">Children & Families</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/what-we-do/supported-living" passHref>
+                    <a className="dropdown-item">Supported Living</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/what-we-do/residential-care" passHref>
+                    <a className="dropdown-item">Residential Care</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/what-we-do/day-services" passHref>
+                    <a className="dropdown-item">Day Services</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/what-we-do/education-training" passHref>
+                    <a className="dropdown-item">Education & Training</a>
+                  </Link>
+                </li>
+              </ul>
             </li>
 
-            {/* How You Can Help (Hover Dropdown) */}
+            {/* How You Can Help (Hybrid Dropdown) */}
             <li className="nav-item dropdown me-3">
-              <Link href="#" passHref>
-                <a className="nav-link dropdown-toggle">How You Can Help</a>
-              </Link>
-              <div className="dropdown-menu">
-                <Link href="/how-you-can-help/volunteer" passHref>
-                  <a className="dropdown-item">Volunteer</a>
-                </Link>
-                <Link href="/how-you-can-help/donate" passHref>
-                  <a className="dropdown-item">Donate</a>
-                </Link>
-                <Link href="/how-you-can-help/fundraising" passHref>
-                  <a className="dropdown-item">Fundraising</a>
-                </Link>
-                <Link href="/how-you-can-help/corporate-partnerships" passHref>
-                  <a className="dropdown-item">Corporate Partnerships</a>
-                </Link>
-              </div>
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                How You Can Help
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <Link href="/how-you-can-help/volunteer" passHref>
+                    <a className="dropdown-item">Volunteer</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/how-you-can-help/donate" passHref>
+                    <a className="dropdown-item">Donate</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/how-you-can-help/fundraising" passHref>
+                    <a className="dropdown-item">Fundraising</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/how-you-can-help/corporate-partnerships" passHref>
+                    <a className="dropdown-item">Corporate Partnerships</a>
+                  </Link>
+                </li>
+              </ul>
             </li>
 
             {/* Careers */}
