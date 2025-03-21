b3971a12e73376182b265b19146d9ba307008b34
SojiJoro <71898347+SojiJoro@users.noreply.github.com>
2025-03-21 22:14:14 +0000

    update

--- src/components/FooterTop.js
+++ src/components/FooterTop.js
@@ -7,27 +7,35 @@ export default function FooterTop() {
     <div style={{ backgroundColor: "#0F354D", color: "#fff", padding: "40px 0" }}>
       <div className="container">
         <div className="row g-4">
-          {/* Column 1: Logo */}
-          <div className="col-md-3">
-            <div className="logo-container d-flex align-items-center justify-content-center">
+          {/* Column 1: Logo with a smaller white background */}
+          <div className="col-md-3 d-flex align-items-center">
+            <div
+              style={{
+                backgroundColor: "#fff",
+                padding: "5px",       // Adjust padding to reduce the white area
+                borderRadius: "6px",  // Optional rounding
+                display: "inline-block",
+              }}
+            >
               <Image
                 src="/images/logo.png"
                 alt="Clear Vital Social Care Logo"
-                width={120}
-                height={120}
+                width={100}          // Smaller logo width
+                height={60}          // Adjust height to match proportion
+                style={{ objectFit: "contain" }}
               />
             </div>
           </div>
 
-          {/* Column 2: LINKS (Top Menus) */}
+          {/* Column 2: Quick Links */}
           <div className="col-md-3">
-            <h5 className="fw-bold mb-3">LINKS</h5>
+            <h5 className="fw-bold mb-3">Quick Links</h5>
             <ul className="list-unstyled">
               <li>
                 <Link href="/" className="text-white">Home</Link>
               </li>
               <li>
-                <Link href="/who-we-are" className="text-white">Who We Are</Link>
+                <Link href="/who-we-are/mission" className="text-white">Who We Are</Link>
               </li>
               <li>
                 <Link href="/what-we-do" className="text-white">What We Do</Link>
@@ -44,9 +52,9 @@ export default function FooterTop() {
             </ul>
           </div>
 
-          {/* Column 3: WHAT WE DO (Sub-Services) */}
+          {/* Column 3: What We Do */}
           <div className="col-md-3">
-            <h5 className="fw-bold mb-3">WHAT WE DO</h5>
+            <h5 className="fw-bold mb-3">What We Do</h5>
             <ul className="list-unstyled">
               <li>
                 <Link href="/what-we-do/adult-social-care" className="text-white">
@@ -60,23 +68,23 @@ export default function FooterTop() {
               </li>
               <li>
                 <Link href="/what-we-do/educational-vocational-support" className="text-white">
-                  Educational & Vocational Support
+                  Educational &amp; Vocational Support
                 </Link>
               </li>
             </ul>
           </div>
 
-          {/* Column 4: GET IN TOUCH */}
+          {/* Column 4: Get in Touch */}
           <div className="col-md-3">
-            <h5 className="fw-bold mb-3">GET IN TOUCH</h5>
+            <h5 className="fw-bold mb-3">Get in Touch</h5>
             <p className="mb-1">29 Alders Road</p>
             <p className="mb-1">Fareham, England</p>
             <p className="mb-1">PO16 0SH</p>
             <p className="mb-1">
               Email:{" "}
-              <a href="mailto:info@clearvitalsocialcare.com" className="text-white">
+              <Link href="mailto:info@clearvitalsocialcare.com" className="text-white">
                 info@clearvitalsocialcare.com
-              </a>
+              </Link>
             </p>
           </div>
         </div>
