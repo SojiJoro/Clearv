80924d3a08f955ddaaaf5d2b1d668964b6b93998
SojiJoro <71898347+SojiJoro@users.noreply.github.com>
2025-03-21 22:36:19 +0000

    update

--- src/app/contact-us/complaints-feedback/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function ComplaintsFeedbackPage() {
-    return (
-      <div>
-        <h1>Complaints and Feedback</h1>
-        <p>Let us know your thoughts...</p>
-      </div>
-    );
-  }
-  
--- src/app/contact-us/locations/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function LocationsPage() {
-    return (
-      <div>
-        <h1>Our Locations</h1>
-        <p>We operate in these areas...</p>
-      </div>
-    );
-  }
-  
